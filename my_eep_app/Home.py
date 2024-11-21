c0038d67c1ab1e9766041d836388376055c22099
elderibeiro <elder.forestview@silomails.com>
2024-11-21 15:37:01 -0600

    candidate updates

--- my_eep_app/Home.py
+++ my_eep_app/Home.py
@@ -1,18 +1,20 @@
 import streamlit as st
 from app.utils import test_mongo_connection
 
-if not test_mongo_connection():
-    st.error("Failed to connect to MongoDB. Please check the database connection and restart the app.")
-else:
-    st.success("MongoDB connection verified!")
-
+#if not test_mongo_connection():
+#    st.error("Failed to connect to MongoDB. Please check the database connection and restart the app.")
+#else:
+#    st.success("MongoDB connection verified!")
 
 def main():
     st.sidebar.title("Navigation")
-    page = st.sidebar.selectbox("Go to", ["Applicant", "Faculty", "Admin"])
+    page = st.sidebar.radio("Go to", ["Welcome", "Applicant", "Faculty", "Admin"])
 
-    if page == "Applicant":
-        from app.pages import Candidate_Application_Page  # Import inside the function to avoid circular import
+    if page == "Welcome":
+        from app.pages import Welcome_Page
+        Welcome_Page.run()
+    elif page == "Applicant":
+        from app.pages import Candidate_Application_Page
         Candidate_Application_Page.run()
     elif page == "Faculty":
         from app.pages import Faculty_Review_Page
