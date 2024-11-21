d209cafc000b4e04295b90c5e897b121971862a5
elderibeiro <elder.forestview@silomails.com>
2024-11-21 17:02:24 -0600

    merge from main

--- my_eep_app/app/pages/Welcome_Page.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-def run():
-    st.title("Welcome to the Enterprise Engineer Programme (EEP)")
-    st.write("""
-        The Enterprise Engineer Programme (EEP) is designed to equip candidates with the skills and knowledge necessary to excel in the engineering field. 
-        This program offers a comprehensive curriculum, hands-on projects, and mentorship opportunities.
-    """)
-    
